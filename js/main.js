eb5ffde5ee1442f27680355977cb98ed3b47e746
Adam <adam.gedney@gmail.com>
2014-01-13 18:05:22 -0500

    added experimental SASS grid system mixin

--- js/main.js
+++ js/main.js
@@ -16,12 +16,12 @@ function init(){
 
 	$.get('templates/templates.html', function(htmlArg){
 
-		var source = $(htmlArg).find('#logged_in').html();
+		var source = $(htmlArg).find('#logged_out').html();
 		var template = Handlebars.compile(source);
 		// var context = {id: posts._id, title:posts.title, created: posts.created, author: posts.author, category: posts.category, text: posts.text}
 		// var html = template(context);
 
-		// $('#content').append(template);
+		$('#content').append(template);
 
 		//defaults to hide upon program load
 		$('.transport_popup').hide();
